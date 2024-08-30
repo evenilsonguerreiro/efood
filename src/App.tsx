1a1e43ed98b35370a23d277d7b44bc8cd2a5f24b
evenilson <evenilsonguerreiro@gmail.com>
2024-08-30 10:53:59 -0300

    inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import { EstiloGlobal } from './styles'
-import Home from './pages/Home'
-import Footer from './components/Footer'
-import Perfil from './pages/Perfil'
-
-const Rotas = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/perfil" element={<Perfil />} />
-  </Routes>
-)
+import Hero from './components/Hero'
 
 function App() {
-  const location = useLocation()
-
-  const isPerfilPage = location.pathname === '/perfil'
-
   return (
     <>
       <EstiloGlobal />
-      {!isPerfilPage && <Header />}
-      <Rotas />
-      {!isPerfilPage && <Footer />}
+      <div className="container">
+        <Header />
+      </div>
+      <Hero />
     </>
   )
 }
-
-export default function Root() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  )
-}
+export default App
--- src/components/ProductList2/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.section``
-
-export const List2 = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-`
